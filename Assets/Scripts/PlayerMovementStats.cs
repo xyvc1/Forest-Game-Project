fa21116d751bfdcb0aa3e527dc49891db5ce84df
CharlieMcLeod <chazmcleod07@gmail.com>
2025-06-16 22:57:10 +0100

    adding new scripts for movement

--- Assets/Scripts/PlayerMovementStats.cs
+++ Assets/Scripts/PlayerMovementStats.cs
@@ -1,16 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovementStats : MonoBehaviour
+[CreateAssetMenu(menuName = "Player Movement")]
+public class PlayerMovementStats : ScriptableObject
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [Header("Walk")]
+    [Range(1f, 100f)] public float MaxWalkSpeed = 12.5f;
+    [Range(0.25f, 50f)] public float GroundAcceleration = 5f;
+    [Range(0.25f, 50f)] public float GroundDeceleration = 20f;
+    [Range(0.25f, 50f)] public float AirAcceleration = 5f;
+    [Range(0.25f, 50f)] public float AirDeceleration = 5f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [Header("Run")]
+    [Range(1f, 100f)] public float MaxRunSpeed = 20f;
+
+    [Header("Grounded/Collision Checks")]
+    public LayerMask GroundLayer;
+    public float GroundDetectionRayLength = 0.02f;
+    public float HeadDetectionRayLength = 0.02f;
+    [Range(0f, 1f)] public float HeadWidth = 0.75f;
 }
